9f4d6be5cce45b0b383b249fd3d0b5c9246cb51a
Inumidun Amao <inumidun.amao@andela.com>
2017-03-28 23:52:31 +0100

    chore(setup-databse): add the dialectoPTIONS.SSL TO TRUE [142469863]

--- server/models/index.js
+++ server/models/index.js
@@ -11,7 +11,11 @@ const db = {};
 
 let sequelize = null;
 if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: true
+    }
+  });
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
