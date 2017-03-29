c1d230f85d6e796af978eedaf7238edb868d5fd4
Inumidun Amao <inumidun.amao@andela.com>
2017-03-29 20:35:00 +0100

    chore(setup-documentation): fix database connection issue[142483485]

--- server/models/index.js
+++ server/models/index.js
@@ -10,12 +10,8 @@ const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
 
 let sequelize = null;
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-      ssl: true
-    }
-  });
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
