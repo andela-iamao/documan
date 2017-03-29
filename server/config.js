fc5e91b2b0eece4f222ff71b21bedb8ebe99fc72
Inumidun Amao <inumidun.amao@andela.com>
2017-03-30 00:15:23 +0100

    chore(setup-documentation): fix cors issues[142483485]

--- server/config.js
+++ server/config.js
@@ -1,12 +1,16 @@
 import bodyParser from 'body-parser';
 import express from 'express';
-import swaggerJSDoc from 'swagger-jsdoc';
 import routes from './routes/index';
 
 module.exports = (app) => {
   app.set('json spaces', 4);
   app.use(express.static('server/public/docs'));
   app.use(bodyParser.json());
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
   routes(app);
   app.use(express.static('server/public/docs'));
   return app;
