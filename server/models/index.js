36c05ce6e0fd18556834788b47f76feee9057691
Inumidun Amao <inumidun.amao@andela.com>
2017-03-29 18:05:51 +0100

    chore(setup-documentation): fix database connection issues[142483485]

--- server/models/index.js
+++ server/models/index.js
@@ -11,7 +11,8 @@ const db = {};
 
 let sequelize = null;
 if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
+  console.log('running dequelize on production');
+  sequelize = new Sequelize('postgres://pxisbnzbniigwu:c0044e5c83851252180bc5c32de917282fda05809c1b478356b0ca31ff9d93db@ec2-107-22-223-6.compute-1.amazonaws.com:5432/d3gk5ajj3np9u5', {
     dialectOptions: {
       ssl: true
     }
