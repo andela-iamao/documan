b9c3a3cc5048afa74ec6aba1b73f0b9f27d9e3e2
Inumidun Amao <inumidun.amao@andela.com>
2017-03-29 17:30:26 +0100

    chore(setup-documentation): fix travis build fail[142483485]

--- server/routes/user.js
+++ server/routes/user.js
@@ -34,33 +34,23 @@ module.exports = () => {
    *         example: greater-than-8
    */
 
-/**
- * @swagger
- * user/create:
- *   post:
- *     tags:
- *       - iamdocuman
- *     description: Creates a new user
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: body
- *         description: user's first name
- *         in: body
- *         required: true
- *         schema:
- *           $ref: '#/definitions/NewUser'
- *     responses:
- *       200:
- *         description: User successfully created
- */
-  router.post('/user/create', (req, res) => {
-    db.Users.create(req.body)
-    .then((result) => {
-      res.json(result);
-    }).catch((err) => {
-      res.json({ status: 'error', msg: err.message });
-    });
+  /**
+   * @swagger
+   * api/v1/users:
+   *   get:
+   *     tags:
+   *       - iamdocuman
+   *     description: Fetches all users
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: User successfully created
+   */
+  router.get('/api/v1/users', (req, res) => {
+    db.Users.findAll({})
+      .then(result => res.json(result))
+      .catch(error => res.status(412).json({ msg: error.message }));
   });
   return router;
 };
---
+++ tests/server/routes/user.spec.js
@@ -0,0 +1,28 @@
+describe('Routes: user', () => {
+  const User = db.Users;
+  beforeEach((done) => {
+    User.destroy({ where: {} })
+      .then(() => {
+        User.create({
+          lastname: "Riddle",
+          username: "tomrid",
+          email: "lordvold@gmail.com",
+          password: "fuck the potters",
+          firstname: "Thomas"
+        }).then(() => done());
+      });
+  });
+
+  describe("GET /api/v1/users", () => {
+    describe("Status 200", () => {
+      it("returns a list of all users", done => {
+        request.get('/api/v1/users')
+          .expect(200)
+          .end((err, res) => {
+            expect(res.body).to.have.length(1);
+            done(err);
+          });
+      });
+    });
+  });
+});
