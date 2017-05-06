eb44808c06c7d037674de34daace159dbc7eea47
Inumidun Amao <inumidun.amao@andela.com>
2017-05-06 12:21:49 +0100

    feature(paginate-table): move db connection url to environment variables[144919429]

--- server/models/index.js
+++ server/models/index.js
@@ -2,19 +2,19 @@
 import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
+import config from '../config/config';
 /* eslint-enable */
 
 const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
+const sequelize = new Sequelize(config.url, config);
 
-let sequelize = null;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env.NODE_ENV[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// let sequelize = null;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env.NODE_ENV[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
 sequelize
   .authenticate()
